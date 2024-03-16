386ffb7c24ff3649b00b83cc5e4184f92e8f0948
saluumaa <salumahassan02@gmail.com>
2024-03-16 20:14:22 +0300

    Create usefetch to add api urls

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,38 @@
+
+import axios from "axios";
+import { useState, useEffect} from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(url);
+                setData(response.data);
+            } catch (error) {
+                setError(true);
+            }
+            setLoading(false);
+        }
+        fetchData();
+    }, [url])
+    
+    const reFetch = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get(url);
+            setData(response.data);
+        } catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+    }
+
+    return {data, loading, error, reFetch};
+};
+
+export default useFetch;
